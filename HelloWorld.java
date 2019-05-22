5a0897568035e290dad363347c1b228e01f2198a
Samir Amanov <samanov@samanov01.rtps.local>
2019-05-22 14:39:48 +0400

    commiting helloworld project

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <=10; i++) {
+			System.out.println(i+". Hello World");
+		}
+	}	
+}
