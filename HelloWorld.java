a8479f4de1874d4463170a218e0b17f52d468207
Samir Amanov <samanov@samanov01.rtps.local>
2019-05-22 15:11:41 +0400

    third commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		for(int i = 1; i <=13; i++) {
-			System.out.println(i+". Hello World and Morld");
+		for(int i = 1; i <=5; i++) {
+			System.out.println(i+". Hello World and Morld and Borld");
 		}
 	}	
 }
