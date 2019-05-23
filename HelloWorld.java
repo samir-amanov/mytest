3140673912064d8198456b410c2b67e0637bcdc5
Samir Amanov <samanov@samanov01.rtps.local>
2019-05-23 10:27:33 +0400

    adding good afternoon

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		for(int i = 1; i <=5; i++) {
 			System.out.println(i+". Hello World and Morld and Borld");
 			System.out.println(i+". Good Morning");
+			System.out.println(i+". Good Afternoon");
 		}
 	}	
 }
