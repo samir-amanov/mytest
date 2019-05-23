67fcb363d07bb4f5eaaf687597daba7f11f0c0a3
Samir Amanov <samanov@samanov01.rtps.local>
2019-05-23 15:52:36 +0400

    evening commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 			System.out.println(i+". Hello World and Morld and Borld");
 			System.out.println(i+". Good Morning");
 			System.out.println(i+". Good Afternoon");
+			System.out.println(i+". Good Evening");
 			System.out.println("=====================================");
 
 		}
